627145ac4be71d3375c740980bb48ec43d0a15c6
armand-bba <armand.barbry@gmail.com>
2026-03-02 18:45:28 +0100

    Update config.js
    Removing the slash at the of the internal domain URL from Weweb

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-  domainSource: "https://1bc7117f-d3a1-43c5-93f7-efde5d324d63.weweb-preview.io/", // Your WeWeb app preview link
+  domainSource: "https://1bc7117f-d3a1-43c5-93f7-efde5d324d63.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
           pattern: "^/scpi/[^/]+/[^/]+",
